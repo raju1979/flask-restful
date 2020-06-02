076121da76ac6f3ea68d589b9a079e9a28e88cf3
Rajesh D <dhrajesh1@gmail.com>
2020-06-02 09:10:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,6 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
-    # from db import db
-    # db.init_app(app)
+    from db import db
+    db.init_app(app)
     app.run(port=5000)
